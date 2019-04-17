0740a6263abb1dc7b0c607b463bcd77bc3666dd4
Uros Vucenovic <u_vucenovic@yahoo.com>
2019-04-17 21:07:06 +0200

    update
    adding custom code

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,15 +1,21 @@
-// BUDGET CONTROLLER
-var budgetController = (function(){
-    var Expense = function(id, description, value){
-        this.id = id;
-        this.description = description;
-        this.value = value;
+// Data module for manipulating data
+var dataModule = (function(){
+    class Expense {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        }
     }
-    var Income = function(id, description, value){
-      this.id = id;
-      this.description = description;
-      this.value = value;
+
+    class Income {
+        constructor(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        }
     }
+
     var data = {
         allItems: {
             exp: [],
@@ -18,126 +24,133 @@ var budgetController = (function(){
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
+
     return {
         addItem: function(type, des, val){
             var newItem, ID;
-            
-            // create new ID
+        
             if (data.allItems[type].length > 0){
+                // create new id and it relative to array position
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
-            }      
-            
-            // create new item
+            }
+
+            // make new item based on data type
             if (type === 'exp'){
                 newItem = new Expense(ID, des, val);
-            } else if (type === 'inc');
-                newItem = new Income(ID, des, val); 
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
 
-            // save to array    
+            // add to data array
             data.allItems[type].push(newItem);
 
-            // return new instance
-            return newItem;       
-        }    
+            return newItem;
+        },
+
+        testing: function(){
+            console.log(data);            
+        }
     }
 })();
 
-// UI CONTROLLER
-var UIController = (function(){
-    // if markup is changed we need only change this object
-    var DOMdata = {
+// UI module for manipulating data display
+var interfaceModule = (function(){
+    // make code more independent / if markup changes we need only change this object
+    var DOM = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
+
     return {
         getInput: function(){
             return {
-              type : document.querySelector(DOMdata.inputType).value, //inc or exp
-              description : document.querySelector(DOMdata.inputDescription).value,
-              value : document.querySelector(DOMdata.inputValue).value
-            };            
+                type: document.querySelector(DOM.inputType).value,
+                description: document.querySelector(DOM.inputDescription).value,
+                value: document.querySelector(DOM.inputValue).value
+            };
         },
-        addListItem: function(obj, type){
-            var html, newHtml, element;
 
-            // create html element depending on type
-            // ! only makes it easier to spot the placeholders which will be replaced by the obj parameter
+        addListItem: function(obj, type){
+            var html, element, newHtml;
+            // create html string with placeholders
             if (type === 'inc'){
-                element = DOMdata.incomesContainer;
+                element = DOM.incomeContainer;
                 html = '<div class="item clearfix" id="income-!id!"><div class="item__description">!description!</div><div class="right clearfix"><div class="item__value">!value!</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp'){
-                element = DOMdata.expensesContainer;
+                element = DOM.expensesContainer;
                 html = '<div class="item clearfix" id="expense-!id!"><div class="item__description">!description!</div><div class="right clearfix"><div class="item__value">!value!</ div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
-            // replace placeholder text
-            newHtml = html.replace('!id!', obj.id);
-            newHtml = newHtml.replace('!description!', obj.description);
-            newHtml = newHtml.replace('!value!', obj.value);
+            // replace placeholder text with actual text
+            var replacement = html.replace(/!id!/, obj.id).replace(/!description!/, obj.description).replace(/!value!/, obj.value);
 
-            // insert into the DOM
-            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            // jonas' solution
+            // newHtml = html.replace('!id!', obj.id);
+            // newHtml = newHtml.replace('!description!', obj.description);
+            // newHtml = newHtml.replace('!value!', obj.value);
+
+            // insert html text
+            document.querySelector(element).insertAdjacentHTML('beforeend', replacement);
         },
-        // replace in UI 
-        clearFields: function(){
-            var fields, fieldsArr;
-            fields = document.querySelectorAll(DOMdata.inputDescription + ', ' + DOMdata.inputValue);
-            // fieldsArr = Array.prototype.slice.call(fields);
-            fieldsArr = Array.from(fields);
-            fieldsArr.forEach(function(current, index, array){
-                current.value = '';
-            });
-            fieldsArr[0].focus();   
-        },
-        getDOMdata: function(){
-            return DOMdata;
+
+        // make DOM accessible to other modules
+        getDOM: function(){
+            return DOM;
         }
     };
 })();
 
-// GLOBAL APP CONTROLER
-var controller = (function(budgetCtrl, UICtrl){   
+// global module for managing previous modules
+var globalModule = (function(dataMod, UIMod){
+    let DOM = UIMod.getDOM();
+
     var setEventListeners = function(){
-        var DOM = UICtrl.getDOMdata();
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
         document.addEventListener('keypress', function(e){
-          if (e.key === 'Enter'){
-            ctrlAddItem();         
-          }       
-        })
-    }
+            if (e.key === 'Enter' || e.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+   
     var ctrlAddItem = function(){
         var input, newItem;
+        // 1. get input
+        input = UIMod.getInput();
+        
+        // add item to dataMod
+        newItem = dataMod.addItem(input.type, input.description, input.value);
+        
+        // add item to UIMod
+        UIMod.addListItem(newItem, input.type);
+        
+        // calculate
+        // display
+    };
 
-        // 1. get input data
-        input = UICtrl.getInput();    
-
-        // 2. add item to budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-        // 3. add item to UI
-        UICtrl.addListItem(newItem, input.type);
-        // 4. calculate budget
-
-        UICtrl.clearFields();
-
-        // 5. display budget on UI
-    }
     return {
         init: function(){
-            console.log('App started');
-            setEventListeners();  
+            setEventListeners();
+            console.log('App running');
         }
     };
-})(budgetController, UIController); // these parameters 
-// get passed as 'budgetCtrl' and 'UICtrl'
+})(dataModule, interfaceModule);
 
-controller.init();
+globalModule.init();
+
+
